9b118069892f959b2e4c248e6e21a8310c2a8c5d
Playzer8 <Loubeyre9629@jbdelasalle.com>
2019-03-04 09:48:58 +0100

    Creation

---
+++ src/application/main.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.sql.DriverManager;
+
+public class main 
+{
+	public void start()
+	{
+		Connection connex = DriverManager.getConnection(jbdc:mysql://localhost:3306/)
+	}
+
+}
